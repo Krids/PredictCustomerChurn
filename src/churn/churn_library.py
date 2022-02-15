2c8b9ac93e11c6bb5528a3975859c0f2baa9e285
Felipe Lana Machado <felipe.krids@gmail.com>
2022-02-15 10:55:59 -0300

    feature: Adding the final tests and methods, also cleaning with pylint style de code.

--- src/churn/churn_library.py
+++ src/churn/churn_library.py
@@ -20,14 +20,11 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
 sns.set()
 
 
 class Churn:
-
-    def __init__(self) -> None:
-        pass
+    '''This class performs the studies of churn.'''
 
     def import_data(self, file_name: str) -> pd.DataFrame:
         '''
@@ -106,21 +103,21 @@ class Churn:
         '''
         input:
                 df: pandas dataframe
-                response: string of response name 
+                response: string of response name
                 [optional argument that could be used for naming variables]
 
         output:
-                X_train: X training data
-                X_test: X testing data
+                x_train: x training data
+                x_test: x testing data
                 y_train: y training data
                 y_test: y testing data
         '''
         df = df.copy()
-        X_train, X_test, y_train, y_test = train_test_split(df[response],
+        x_train, x_test, y_train, y_test = train_test_split(df[response],
                                                             df['churn'],
                                                             test_size=0.3,
                                                             random_state=42)
-        return X_train, X_test, y_train, y_test
+        return x_train, x_test, y_train, y_test
 
     def classification_report_image(self,
                                     y_train,
@@ -143,42 +140,58 @@ class Churn:
         output:
                 None
         '''
-        title_list = {'Random Forest Train': [y_train, y_train_preds_rf], 'Random Forest Test': [y_test, y_test_preds_rf],
-                      'Logistic Regression Train': [y_train, y_train_preds_lr], 'Logistic Regression Tests': [y_test, y_test_preds_lr]}
-        for title in title_list:
-                blank_image_lrte = Image.new('RGB', (400, 150))
-                draw_blankIm_lrte = ImageDraw.Draw(blank_image_lrte)
-                draw_blankIm_lrte.text((5, 5), title)
-                draw_blankIm_lrte.text(
+        title_list = {'Random Forest Train': [y_train, y_train_preds_rf],
+                      'Random Forest Test': [y_test, y_test_preds_rf],
+                      'Logistic Regression Train': [y_train, y_train_preds_lr],
+                      'Logistic Regression Tests': [y_test, y_test_preds_lr]}
+        for title, list_of_dfs in title_list.items():
+            blank_image_lrte = Image.new('RGB', (400, 150))
+            draw_blankim_lrte = ImageDraw.Draw(blank_image_lrte)
+            draw_blankim_lrte.text((5, 5), title)
+            draw_blankim_lrte.text(
                 (15, 15), classification_report(
-                        title_list[title][0], title_list[title][1]))
-                blank_image_lrte.save(os.path.join(IMAGES_EDA, f"{title.replace(' ', '_')}.png"))
+                    list_of_dfs[0], list_of_dfs[1]))
+            blank_image_lrte.save(os.path.join(
+                IMAGES_EDA, f"{title.replace(' ', '_')}.png"))
 
-    def feature_importance_plot(self, model, X_data, output_pth) -> None:
+    def feature_importance_plot(self, model, x_data: pd.DataFrame) -> None:
         '''
-        creates and stores the feature importances in pth
+        Creates and stores the feature importances
         input:
                 model: model object containing feature_importances_
-                X_data: pandas dataframe of X values
-                output_pth: path to store the figure
+                x_data: pandas dataframe of X values
 
         output:
                 None
         '''
+        importances = model.feature_importances_
 
-    def train_models(self, X_train, X_test, y_train, y_test) -> None:
+        indices = np.argsort(importances)[::-1]
+        names = [x_data.columns[i] for i in indices]
+
+        plt.figure(figsize=(20, 5))
+        plt.title("Feature Importance")
+        plt.ylabel('Importance')
+
+        plt.bar(range(x_data.shape[1]), importances[indices])
+
+        plt.xticks(range(x_data.shape[1]), names, rotation=90)
+
+        plt.savefig(os.path.join(IMAGES_RESULTS, 'Feature_Importance.png'))
+
+    def train_models(self, x_train, x_test, y_train, y_test) -> None:
         '''
         train, store model results: images + scores, and store models
         input:
-                X_train: X training data
-                X_test: X testing data
+                x_train: X training data
+                x_test: X testing data
                 y_train: y training data
                 y_test: y testing data
         output:
                 None
         '''
-        X_train = X_train.copy()
-        X_test = X_test.copy()
+        x_train = x_train.copy()
+        x_test = x_test.copy()
         y_train = y_train.copy()
         y_test = y_test.copy()
 
@@ -186,27 +199,53 @@ class Churn:
         rfc = RandomForestClassifier(random_state=42)
         lrc = LogisticRegression()
 
-        param_grid = { 
-        'n_estimators': [200, 500],
-        'max_features': ['auto', 'sqrt'],
-        'max_depth' : [4,5,100],
-        'criterion' :['gini', 'entropy']
+        param_grid = {
+            'n_estimators': [200, 500],
+            'max_features': ['auto', 'sqrt'],
+            'max_depth': [4, 5, 100],
+            'criterion': ['gini', 'entropy']
         }
 
         cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
-        cv_rfc.fit(X_train, y_train)
+        cv_rfc.fit(x_train, y_train)
 
-        lrc.fit(X_train, y_train)
+        lrc.fit(x_train, y_train)
 
-        y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
-        y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+        y_train_preds_rf = cv_rfc.best_estimator_.predict(x_train)
+        y_test_preds_rf = cv_rfc.best_estimator_.predict(x_test)
 
-        y_train_preds_lr = lrc.predict(X_train)
-        y_test_preds_lr = lrc.predict(X_test)
+        y_train_preds_lr = lrc.predict(x_train)
+        y_test_preds_lr = lrc.predict(x_test)
+
+        self.feature_importance_plot(
+            model=cv_rfc.best_estimator_, x_data=pd.concat([x_train, x_test], axis=0))
 
         # save best model
-        joblib.dump(cv_rfc.best_estimator_, os.path.join(DOCS_MODELS, 'rfc_model.pkl'))
+        joblib.dump(cv_rfc.best_estimator_, os.path.join(
+            DOCS_MODELS, 'rfc_model.pkl'))
         joblib.dump(lrc, os.path.join(DOCS_MODELS, 'logistic_model.pkl'))
+
+        self.classification_report_image(y_train,
+                                         y_test,
+                                         y_train_preds_lr,
+                                         y_train_preds_rf,
+                                         y_test_preds_lr,
+                                         y_test_preds_rf)
+
+        lrc_plot = plot_roc_curve(lrc, x_test, y_test)
+        plt.figure(figsize=(15, 8))
+        ax = plt.gca()
+        _ = plot_roc_curve(
+            cv_rfc.best_estimator_, x_test, y_test, ax=ax, alpha=0.8)
+        lrc_plot.plot(ax=ax, alpha=0.8)
+        plt.savefig(os.path.join(IMAGES_RESULTS, 'ROC_curve_best_models.png'))
+        plt.close()
+
+        plt.figure(figsize=(15, 8))
+        explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
+        shap_values = explainer.shap_values(x_test)
+        shap.summary_plot(shap_values, x_test, plot_type="bar", show=False)
+        plt.savefig(os.path.join(IMAGES_RESULTS, 'SHAP_random_forest.png'))
 
         # scores
         print('random forest results')
--- tests/test_churn_library.py
+++ tests/test_churn_library.py
@@ -7,15 +7,15 @@ Date: 06/02/2022
 import os
 import logging as log
 import unittest
+import joblib
+import pandas as pd
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
 from src.churn.churn_library import Churn
-from src.helpers.project_paths import DOCS_LOGS, IMAGES_EDA
+from src.helpers.project_paths import DOCS_LOGS, DOCS_MODELS, IMAGES_EDA, IMAGES_RESULTS
 
 
 class TestChurn(unittest.TestCase):
+    '''This class is responsible to perform the tests in the churn library class.'''
 
     def setUp(self) -> None:
         self.churn = Churn()
@@ -59,27 +59,43 @@ class TestChurn(unittest.TestCase):
 
     def test_eda(self):
         '''
-        Test the perform eda function, looking to assert if there is files in the images/eda folder after the execution on the program.
+        Test the perform eda function, looking to assert if there is files in the images/eda
+        folder after the execution on the program.
 
         Raises:
             err: Raises an AssertionError if there is no files in image folders.
         '''
         df = self.churn.import_data("bank_data.csv")
+        df = self.churn.perform_eda(df=df)
+        list_of_files = os.listdir(IMAGES_EDA)
+        images_of_distribuition = len(
+            [file for file in list_of_files if '_distribuition' in file])
+        images_of_heatmap = len(
+            [file for file in list_of_files if 'heatmap' in file])
         try:
-            df = self.churn.perform_eda(df=df)
-            list = os.listdir(IMAGES_EDA)
-            number_files = len(list)
-            self.assertTrue(number_files == 5)
+            self.assertTrue(images_of_distribuition == 4)
             log.info(
-                f"SUCCESS on perform_eda: The is {number_files} files in images EDA directory.")
+                (f"SUCCESS on perform_eda: The is {images_of_distribuition}"
+                " files for distribuition in images EDA directory."))
         except AssertionError as err:
             log.error(
-                f"ERROR on perform_eda: There is {number_files} files inside the images directory.")
+                ("ERROR on perform_eda: There isn`t 4 files for distribuition"
+                " inside the images/eda directory."))
+            raise err
+
+        try:
+            self.assertTrue(images_of_heatmap == 1)
+            log.info(
+                "SUCCESS on perform_eda: The is 1 file of heatmap in images EDA directory.")
+        except AssertionError as err:
+            log.error(
+                ("ERROR on perform_eda: There isn`t 1 files of heatmap"
+                " inside the images/eda directory."))
             raise err
 
     def test_encoder_helper(self):
         '''
-        Test if the columns are encoded correctly, 
+        Test if the columns are encoded correctly,
 
         Raises:
             err: Raises an AssertionError if there is no columns that contains _churn.
@@ -91,13 +107,15 @@ class TestChurn(unittest.TestCase):
             df = self.churn.encoder_helper(df=df, category_list=list(
                 df.select_dtypes(include=['object']).columns), response='churn')
             has_churn_rate_columns = any(
-                [True if x.find('_churn') != -1 else False for x in list(df.columns)])
+                [bool(x.find('_churn') != -1) for x in list(df.columns)])
             self.assertTrue(has_churn_rate_columns)
             log.info(
-                f"SUCCESS on encoder_helper(): There is columns containing the name _chrun in the dataframe.")
+                ("SUCCESS on encoder_helper(): There is columns containing"
+                " the name _chrun in the dataframe."))
         except AssertionError as err:
             log.error(
-                f"ERROR on encoder_helper(): There isn`t any columns containing the name _chrun in the dataframe.")
+                ("ERROR on encoder_helper(): There isn`t any columns"
+                " containing the name _chrun in the dataframe."))
             raise err
 
     def test_perform_feature_engineering(self):
@@ -118,10 +136,10 @@ class TestChurn(unittest.TestCase):
                                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
                                 'Gender_churn', 'Education_Level_churn', 'Marital_Status_churn',
                                 'Income_Category_churn', 'Card_Category_churn']
-        X_train, X_test, y_train, y_test = self.churn.perform_feature_engineering(
+        x_train, x_test, y_train, y_test = self.churn.perform_feature_engineering(
             df=df, response=columns_for_training)
         try:
-            self.assertFalse(X_train.empty)
+            self.assertFalse(x_train.empty)
             self.assertFalse(y_train.empty)
             log.info(
                 'SUCCESS on perform_feature_engineering: Train data was created and not empty.')
@@ -132,7 +150,7 @@ class TestChurn(unittest.TestCase):
             raise err
 
         try:
-            self.assertFalse(X_test.empty)
+            self.assertFalse(x_test.empty)
             self.assertFalse(y_test.empty)
             log.info(
                 'SUCCESS on perform_feature_engineering: Test data was created and not empty.')
@@ -143,7 +161,7 @@ class TestChurn(unittest.TestCase):
             raise err
 
         try:
-            self.assertEqual(X_train.shape[0] == y_train.shape[0])
+            self.assertTrue(x_train.shape[0] == y_train.shape[0])
             log.info(
                 'SUCCESS on perform_feature_engineering: Train data shapes match.')
 
@@ -153,19 +171,102 @@ class TestChurn(unittest.TestCase):
             raise err
 
         try:
-            self.assertEqual(X_test.shape[0] == y_test.shape[0])
+            self.assertTrue(x_test.shape[0] == y_test.shape[0])
             log.info(
-                'SUCCESS on perform_feature_engineering: Train data shapes match.')
+                'SUCCESS on perform_feature_engineering: Test data shapes match.')
 
         except AssertionError as err:
             log.error(
                 'ERROR on perform_feature_engineering: Test data X and Y doesnt match.')
             raise err
 
+    def test_train_models(self):
+        '''
+        This function test if the train_models method save models correctly and store the
+        images of the results in the properly location.
+
+        Raises:
+            err: Raises an AssertionError if there is no files in image folders.
+        '''
+        df = self.churn.import_data("bank_data.csv")
+        df = self.churn.perform_eda(df=df)
+        df = self.churn.encoder_helper(df=df, category_list=list(
+            df.select_dtypes(include=['object']).columns), response='churn')
+        columns_for_training = ['Customer_Age', 'Dependent_count', 'Months_on_book',
+                                'Total_Relationship_Count', 'Months_Inactive_12_mon',
+                                'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
+                                'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
+                                'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
+                                'Gender_churn', 'Education_Level_churn', 'Marital_Status_churn',
+                                'Income_Category_churn', 'Card_Category_churn']
+        x_train, x_test, y_train, y_test = self.churn.perform_feature_engineering(
+            df=df, response=columns_for_training)
+
+        self.churn.train_models(
+            x_train=x_train, x_test=x_test, y_train=y_train, y_test=y_test)
+
+        list_of_files = os.listdir(DOCS_MODELS)
+        model_of_rfc = len(
+            [file for file in list_of_files if 'rfc_model' in file])
+
+        model_of_logistic = len(
+            [file for file in list_of_files if 'logistic_model' in file])
+
+        try:
+            self.assertEqual(model_of_rfc, 1)
+            log.info(
+                ("SUCCESS on train_models: There is one model of"
+                " Random Forest on docs/models folder."))
+        except AssertionError as err:
+            log.error(
+                ("ERROR on train_models: There isn`t one model of"
+                " Random Forest on docs/models folder."))
+            raise err
+
+        try:
+            self.assertEqual(model_of_logistic, 1)
+            log.info(
+                ('SUCCESS on train_models: There is one model of'
+                ' Logistic Regression on docs/models folder.'))
+        except AssertionError as err:
+            log.error(
+                ('ERROR on train_models: There isn`t one model of'
+                ' Logistic Regression on docs/models folder.'))
+            raise err
+
+        list_of_files = os.listdir(IMAGES_RESULTS)
+        image_of_roc_curve = len(
+            [file for file in list_of_files if 'ROC_curve_best_models' in file])
+
+        image_of_shap_values = len(
+            [file for file in list_of_files if 'SHAP_random_forest' in file])
+
+        try:
+            self.assertEqual(image_of_roc_curve, 1)
+            log.info(
+                ('SUCCESS on train_models: There is one image for ROC'
+                ' curve on images/results folder.'))
+        except AssertionError as err:
+            log.error(
+                ('ERROR on train_models: There isn`t one image for'
+                ' ROC curve on images/results folder.'))
+            raise err
+
+        try:
+            self.assertEqual(image_of_shap_values, 1)
+            log.info(
+                ('SUCCESS on train_models: There is one image for SHAP values on'
+                ' the Randon Forest on images/results folder.'))
+        except AssertionError as err:
+            log.error(
+                ('ERROR on train_models: There isn`t one image for SHAP values on'
+                ' the Randon Forest on images/results folder.'))
+            raise err
+
     def test_classification_report(self):
         """
-        Tests if the function produces classification report for training and testing results and stores report as image
-        in images/eda folder.
+        Tests if the function produces classification report for training and
+        testing results and stores report as image in images/eda folder.
 
         Raises:
             err: Raises an AssertionError if there is no files in images/eda folder.
@@ -181,30 +282,17 @@ class TestChurn(unittest.TestCase):
                                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
                                 'Gender_churn', 'Education_Level_churn', 'Marital_Status_churn',
                                 'Income_Category_churn', 'Card_Category_churn']
-        X_train, X_test, y_train, y_test = self.churn.perform_feature_engineering(
+        x_train, x_test, y_train, y_test = self.churn.perform_feature_engineering(
             df=df, response=columns_for_training)
-        
-        # grid search
-        rfc = RandomForestClassifier(random_state=42)
-        lrc = LogisticRegression()
 
-        param_grid = { 
-            'n_estimators': [200, 500],
-            'max_features': ['auto', 'sqrt'],
-            'max_depth' : [4,5,100],
-            'criterion' :['gini', 'entropy']
-        }
+        rfc_model = joblib.load(os.path.join(DOCS_MODELS, 'rfc_model.pkl'))
+        lr_model = joblib.load(os.path.join(DOCS_MODELS, 'logistic_model.pkl'))
 
-        cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
-        cv_rfc.fit(X_train, y_train)
+        y_train_preds_rf = rfc_model.predict(x_train)
+        y_test_preds_rf = rfc_model.predict(x_test)
 
-        lrc.fit(X_train, y_train)
-
-        y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
-        y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
-
-        y_train_preds_lr = lrc.predict(X_train)
-        y_test_preds_lr = lrc.predict(X_test)
+        y_train_preds_lr = lr_model.predict(x_train)
+        y_test_preds_lr = lr_model.predict(x_test)
 
         self.churn.classification_report_image(y_train,
                                                y_test,
@@ -222,47 +310,33 @@ class TestChurn(unittest.TestCase):
         try:
             self.assertEqual(images_random_forest_length, 2)
             log.info(
-                f'SUCCESS on classification_report: There is two images of Random Forest (Train and Test) on images/eda folder.')
+                ('SUCCESS on classification_report: There is two images of'
+                ' Random Forest (Train and Test) on images/eda folder.'))
         except AssertionError as err:
             log.error(
-                f'ERROR on classification_report: There isn`t two images of Random Forest (Train and Test) on images/eda folder.')
+                ('ERROR on classification_report: There isn`t two images of'
+                ' Random Forest (Train and Test) on images/eda folder.'))
             raise err
 
         try:
             self.assertEqual(images_logistic_regression_length, 2)
             log.info(
-                f'SUCCESS on classification_report: There is two images of Logistic Regression (Train and Test) on images/eda folder.')
+                ('SUCCESS on classification_report: There is two images of'
+                ' Logistic Regression (Train and Test) on images/eda folder.'))
         except AssertionError as err:
             log.error(
-                f'ERROR on classification_report: There isn`t two images of Logistic Regression (Train and Test) on images/eda folder.')
+                ('ERROR on classification_report: There isn`t two images of'
+                ' Logistic Regression (Train and Test) on images/eda folder.'))
             raise err
 
     def test_feature_importance_plot(self):
         """
-        Tests if the function produces classification report for training and testing results and stores report as image
-        in images/results folder.
+        Tests if the function produces classification report for training and
+         testing results and stores report as image in images/results folder.
 
         Raises:
             err: Raises an AssertionError if there is no files in images/result folder.
         """
-        
-
-        try:
-            self.assertEqual(images_logistic_regression_length, 2)
-            log.info(
-                f'SUCCESS on classification_report: There is two images of Logistic Regression (Train and Test) on images/eda folder.')
-        except AssertionError as err:
-            log.error(
-                f'ERROR on classification_report: There isn`t two images of Logistic Regression (Train and Test) on images/eda folder.')
-            raise err
-
-    def test_train_models(self):
-        '''
-        test train_models
-
-        Raises:
-            err: Raises an AssertionError if there is no files in image folders.
-        '''
         df = self.churn.import_data("bank_data.csv")
         df = self.churn.perform_eda(df=df)
         df = self.churn.encoder_helper(df=df, category_list=list(
@@ -274,17 +348,24 @@ class TestChurn(unittest.TestCase):
                                 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
                                 'Gender_churn', 'Education_Level_churn', 'Marital_Status_churn',
                                 'Income_Category_churn', 'Card_Category_churn']
-        X_train, X_test, y_train, y_test = self.churn.perform_feature_engineering(
+        x_train, x_test, _, _ = self.churn.perform_feature_engineering(
             df=df, response=columns_for_training)
-        df = self.churn.train_models(
-            X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
-        try:
-            assert X_train.empty == False
-            assert y_train.empty == False
-            log.info(
-                'SUCCESS on train_models: Train data was created and not empty.')
 
+        rfc_model = joblib.load(os.path.join(DOCS_MODELS, 'rfc_model.pkl'))
+
+        self.churn.feature_importance_plot(
+            model=rfc_model, x_data=pd.concat([x_train, x_test], axis=0))
+
+        list_of_files = os.listdir(IMAGES_RESULTS)
+        images_random_forest_importance_length = len(
+            [file for file in list_of_files if 'Feature_Importance' in file])
+        try:
+            self.assertEqual(images_random_forest_importance_length, 1)
+            log.info(
+                ('SUCCESS on feature_importance_plot: There is a image of'
+                ' Random Forest features importance on images/results folder.'))
         except AssertionError as err:
             log.error(
-                'ERROR on train_models: Train data could not be created')
+                ('ERROR on feature_importance_plot: There isn`t a image of'
+                ' Random Forest features importance on images/results folder.'))
             raise err
